d0ffdfdd9c0b98c65558e8e067a22c321f73dbda
KenjiDuggan <kenjiduggan@gmail.com>
2021-03-22 22:40:07 -0400

    queue and stack implementations from linked lists

---
+++ src/DataStructures/Stacks/LinkedStack.java
@@ -0,0 +1,14 @@
+package DataStructures.Stacks;
+
+import DataStructures.Interfaces.Stack;
+import DataStructures.Lists.SinglyLinkedList;
+
+public class LinkedStack<E> implements Stack<E> {
+    private SinglyLinkedList<E> list = new SinglyLinkedList<>();
+    public LinkedStack() {}
+    public int size() { return list.size(); }
+    public boolean isEmpty() { return list.isEmpty(); }
+    public void push(E element) { list.addFirst(element); }
+    public E top() { return list.first(); }
+    public E pop() { return list.removeFirst(); }
+}
